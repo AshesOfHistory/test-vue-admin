80e5a04cfb18de6f3a4907c23d06f2649f668fe2
wilson_wp <842104779@qq.com>
2019-08-06 14:38:38 +0800

    driver.js引入，readme文档添加项目结构目录

--- README.md
+++ README.md
@@ -1,21 +1,1 @@
-# test-vue
-
-> A Vue.js project
-
-## Build Setup
-
-``` bash
-# install dependencies
-npm install
-
-# serve with hot reload at localhost:8080
-npm run dev
-
-# build for production with minification
-npm run build
-
-# build for production and view the bundle analyzer report
-npm run build --report
-```
-
-For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+﻿﻿﻿﻿﻿# test-vue> 项目结构目录```bash├── build|  ├── build.js|  ├── check-versions.js|  ├── logo.png|  ├── utils.js|  ├── vue-loader.conf.js|  ├── webpack.base.conf.js|  ├── webpack.dev.conf.js|  └── webpack.prod.conf.js├── config|  ├── dev.env.js|  ├── index.js|  └── prod.env.js├── index.html├── package-lock.json├── package.json├── README.md├── src|  ├── api|  ├── App.vue|  ├── assets|  ├── components|  ├── icons|  ├── layout|  ├── main.js|  ├── router|  ├── store|  ├── stylus|  ├── utils|  └── views├── static```## 运行步骤``` bash# install dependenciesnpm install# serve with hot reload at localhost:8080npm run dev# build for production with minificationnpm run build# build for production and view the bundle analyzer reportnpm run build --report```
--- src/views/form/holiday.vue
+++ src/views/form/holiday.vue
@@ -1,27 +1,72 @@
 <template>
   <div class="holiday">
     请假申请表
+
+    <div id="test1">引导测试1</div>
+    <div id="test2">引导测试2</div>
+
+    <el-button @click="guide">开始引导</el-button>
   </div>
 </template>
 
 <script>
+  import Driver from 'driver.js' // import driver.js
+
   export default {
-    model: {},
-    props: {},
-    components: {},
-    created() {
-    },
     mounted() {
     },
     data() {
-      return {}
+      return {
+        steps:[
+          {
+            element:'#test1',
+            popover:{
+              title: '测试标题1',
+              description: '测试说明1',
+              position: 'bottom',
+              nextBtnText: '下一步',
+              prevBtnText: '上一步',
+              doneBtnText: '完成',
+              closeBtnText: '关闭'
+            }
+          },
+          {
+            element:'#test2',
+            popover:{
+              title: '测试标题2',
+              description: '测试说明2',
+              position: 'bottom',
+              nextBtnText: '下一步',
+              prevBtnText: '上一步',
+              doneBtnText: '完成',
+              closeBtnText: '关闭'
+            },
+            padding:0
+          },
+        ]
+      }
     },
-    methods: {},
-    computed: {},
+    methods: {
+      guide(){
+        const driver = new Driver()
+        // driver.highlight({
+        //   element: '#test1',
+        //   popover:{
+        //     title: '测试标题1',
+        //     description: '测试说明1',
+        //     position: 'bottom',
+        //   }
+        // })
+        driver.defineSteps(this.steps)
+        driver.start()
+      }
+    },
   }
 </script>
 
 <style lang="scss" scoped>
+  @import "../../../node_modules/driver.js/dist/driver.min.css";
   .holiday {
   }
+
 </style>
